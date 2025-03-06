90002f27354fd7fa66e6c309863aad88df54301a
Kenneth Brewer <101129262+kenbrew96@users.noreply.github.com>
2025-03-06 12:33:11 -0500

    Create list_methods.py

---
+++ list_methods.py
@@ -0,0 +1,12 @@
+movies = ["Avatar", "Titanic", "Avengers"]
+movies.append("Alien")
+print(movies[3])
+
+colors = ["Red", "Blue", "Yellow"]
+colors.insert(2, "Green")
+colors.append("Black")
+print(colors[3])
+
+topic = ["Maths", "English", "Physics"]
+topic.pop(2)
+print(topic)
